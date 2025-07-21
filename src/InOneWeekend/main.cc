5ced614eb5d1ccbdd4fb49032991b979c3ab369e
Ana Clara Matta <anaclaramatta87@gmail.com>
2025-07-21 19:28:11 -0300

    Adicionei o rosto

--- src/InOneWeekend/main.cc
+++ src/InOneWeekend/main.cc
@@ -21,45 +21,37 @@
 int main() {
     hittable_list world;
 
+    //chão
     auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
     world.add(make_shared<sphere>(point3(0,-1000,0), 1000, ground_material));
 
-    for (int a = -11; a < 11; a++) {
-        for (int b = -11; b < 11; b++) {
-            auto choose_mat = random_double();
-            point3 center(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
+    //o círculo do rosto
+    for (int i = 0; i < 25; i++) { //add as bolinhas em círculo
+        double angle = 2 * pi * i/ 25;
+        double x = cos(angle);
+        double z = sin(angle);
 
-            if ((center - point3(4, 0.2, 0)).length() > 0.9) {
-                shared_ptr<material> sphere_material;
+        x += random_double(-0.1, 0.1); //coloca um ruído p ficar - certinho
+        z += random_double(-0.1, 0.1);
 
-                if (choose_mat < 0.8) {
-                    // diffuse
-                    auto albedo = color::random() * color::random();
-                    sphere_material = make_shared<lambertian>(albedo);
-                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
-                } else if (choose_mat < 0.95) {
-                    // metal
-                    auto albedo = color::random(0.5, 1);
-                    auto fuzz = random_double(0, 0.5);
-                    sphere_material = make_shared<metal>(albedo, fuzz);
-                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
-                } else {
-                    // glass
-                    sphere_material = make_shared<dielectric>(1.5);
-                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
-                }
-            }
+        //seleciona um material aleatório pro rosto
+        shared_ptr<material> face_material;
+        auto choose_mat = random_double();
+
+        if (choose_mat < 0.8) { //difuso
+            auto albedo = color::random() * color::random();
+            face_material = make_shared<lambertian>(albedo);
+        } else if (choose_mat < 0.95) { //metal
+            auto albedo = color::random(0.5, 1);
+            auto fuzz = random_double(0, 0.5);
+            face_material = make_shared<metal>(albedo, fuzz);
+        } else { //vidro
+            face_material = make_shared<dielectric>(1.5);
         }
+
+        //add na cena 
+        world.add(make_shared<sphere>(point3(x, 0.01, z), 0.1, face_material));
     }
-
-    auto material1 = make_shared<dielectric>(1.5);
-    world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
-
-    auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
-    world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
-
-    auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
-    world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
 
     camera cam;
 
@@ -68,12 +60,13 @@ int main() {
     cam.samples_per_pixel = 10;
     cam.max_depth         = 20;
 
-    cam.vfov     = 20;
-    cam.lookfrom = point3(13,2,3);
-    cam.lookat   = point3(0,0,0);
-    cam.vup      = vec3(0,1,0);
+    //alterei a câmera pra olhar de cima e tirei o desfoque
+    cam.vfov     = 30;
+    cam.lookfrom = point3(0, 5, 0);
+    cam.lookat   = point3(0, 0, 0);
+    cam.vup      = vec3(0, 0, -1);
 
-    cam.defocus_angle = 0.6;
+    cam.defocus_angle = 0;
     cam.focus_dist    = 10.0;
 
     cam.render(world);
